42cbe966f0797f303bf4e1a223fed322683fefcf
matheusburey <matheusbureidev@gmail.com>
2023-05-29 16:20:27 -0300

    header improvements

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,26 +7,28 @@ interface IProps {
 
 export default function Header({ user }: IProps) {
   return (
-    <header className="py-5 flex justify-around items-center">
-      <div className="flex items-center gap-5">
-        <figure>
-          <Image
-            src={user.avatar_url}
-            alt={user.name}
-            width={50}
-            height={50}
-            className="rounded-full"
-          />
-          <figcaption className="hidden">{user.name}</figcaption>
-        </figure>
-        <h1 className="font-bold text-xl">{user.name.toUpperCase()}</h1>
+    <header className="fixed w-full p-6 bg-white">
+      <div className="max-w-7xl mx-auto flex  justify-between">
+        <div className="flex items-center gap-5">
+          <figure>
+            <Image
+              src={user.avatar_url}
+              alt={user.name}
+              width={50}
+              height={50}
+              className="rounded-full"
+            />
+            <figcaption className="hidden">{user.name}</figcaption>
+          </figure>
+          <h1 className="font-bold text-xl">{user.name.toUpperCase()}</h1>
+        </div>
+        <nav className="flex align-center">
+          <Link href="" text="HOME" />
+          <Link href="#about" text="SOBRE MIM" />
+          <Link href="#projects" text="PROJETOS" />
+          <Link href="#contact" text="CONTATO" />
+        </nav>
       </div>
-      <nav>
-        <Link href="#" text="HOME" />
-        <Link href="#" text="ABOUT" />
-        <Link href="#" text="PROJECTS" />
-        <Link href="#" text="CONTACT" />
-      </nav>
     </header>
   );
 }
