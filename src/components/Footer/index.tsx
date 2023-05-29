91575bbe37ce4040e4f48b27079377f7e50b9870
matheusburey <matheusbureidev@gmail.com>
2023-05-29 16:19:56 -0300

    shift text to center

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -7,30 +7,32 @@ interface IProps {
 
 export default function Footer({ user }: IProps) {
   return (
-    <footer className="bg-black text-white py-16 flex justify-around items-center">
-      <div>
-        <h1 className="font-bold text-xl mb-6">{user.name.toUpperCase()}</h1>
-        <p className="text-sm">{user.bio}</p>
-      </div>
-      <div>
-        <h2 className="font-bold text-xl">SOCIAL</h2>
-        <nav className="flex gap-5 pt-4">
-          <Link href={user.html_url} target="_blank">
-            <AiFillGithub size={25} />
-          </Link>
-          <Link
-            href="https://www.linkedin.com/in/matheus-h-burei/"
-            target="_blank"
-          >
-            <AiFillLinkedin size={25} />
-          </Link>
-          <Link
-            href={`https://twitter.com/${user.twitter_username}`}
-            target="_blank"
-          >
-            <AiOutlineTwitter size={25} />
-          </Link>
-        </nav>
+    <footer className="bg-black text-white py-16 w-full px-6">
+      <div className="max-w-7xl mx-auto flex justify-between">
+        <div className="max-w-md">
+          <h1 className="font-bold text-xl mb-6">{user.name.toUpperCase()}</h1>
+          <p className="text-sm">{user.bio}</p>
+        </div>
+        <div>
+          <h2 className="font-bold text-xl">SOCIAL</h2>
+          <nav className="flex gap-5 pt-4">
+            <Link href={user.html_url} target="_blank">
+              <AiFillGithub size={25} />
+            </Link>
+            <Link
+              href="https://www.linkedin.com/in/matheus-h-burei/"
+              target="_blank"
+            >
+              <AiFillLinkedin size={25} />
+            </Link>
+            <Link
+              href={`https://twitter.com/${user.twitter_username}`}
+              target="_blank"
+            >
+              <AiOutlineTwitter size={25} />
+            </Link>
+          </nav>
+        </div>
       </div>
     </footer>
   );
